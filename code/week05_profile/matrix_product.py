aa57f452fcd6dd63426196e34a98fac5afe9945c
wasit7 <wasit7@gmail.com>
2015-02-11 14:10:03 +0700

    week05 update

--- code/week05_profile/matrix_product.py
+++ code/week05_profile/matrix_product.py
@@ -8,8 +8,10 @@ import numpy as np
 from IPython import parallel
 from k_partition import partition
 c=parallel.Client()
-dv=c.direct_view()
 print c.ids
+dv=c.direct_view()
+dv.execute('import numpy as np')
+
 
 x = np.linspace(0, 7, 8)
 y = np.linspace(0, 7, 8)
@@ -31,10 +33,26 @@ pr=partition(r_par,rmax)
 pc=partition(c_par,cmax)
 ri=0
 for i in xrange(r_par):    
-    rf=ri+pr[i]    
-    c[i*c_par:(i+1)*c_par]['a']=A[ri:rf,:]
+    rf=ri+pr[i]
+    x=i*c_par+np.arange(c_par) 
+    for j in x:
+        c[j]['a']=A[ri:rf,:]
+#    c[i*c_par:(i+1)*c_par]['a']=A[ri:rf,:]
     
-    print 'core ',i*c_par,':',(i+1)*c_par
+    print 'core: ',x
     print 'row ',ri,':',rf
     ri=rf
-print dv['a']
+#print dv['a']
+
+ci=0
+for i in xrange(c_par):    
+    cf=ci+pc[i]
+    x=np.arange(r_par)*c_par+i
+    for j in x:
+        c[j]['b']=B[:,ci:cf]
+    
+    print 'core: ',x
+    print 'column ',ci,':',cf
+    ci=cf
+#print dv['b']
+dv.execute('c=np.dot(a,b)')
