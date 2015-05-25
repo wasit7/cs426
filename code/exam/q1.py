9acd309a5036566417c15eb617070b026d3edc54
wasit7 <wasit7@gmail.com>
2015-05-25 10:19:56 +0700

    removing pic

--- code/exam/q1.py
+++ code/exam/q1.py
@@ -7,7 +7,7 @@ Created on Mon May 25 09:01:43 2015
 import numpy as np
 import pickle
 pfile=open('q1_input.pic','wb')
-A=np.random.rand(2000,4000)
-B=np.random.rand(4000,1000)
+A=np.random.rand(5000,8000)
+B=np.random.rand(8000,9000)
 pickle.dump((A,B),pfile,pickle.HIGHEST_PROTOCOL)
 pfile.close()
