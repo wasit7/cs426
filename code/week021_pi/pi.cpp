afdd0e8e5d3a761f3f6c222e85e5da581d32e682
wasit7 <wasit7@gmail.com>
2015-08-27 18:36:56 +0700

    updated pi.cpp

--- code/week021_pi/pi.cpp
+++ code/week021_pi/pi.cpp
@@ -8,22 +8,22 @@ using namespace std;
 #define N_cores 8
 int main(void){
         unsigned long long sum[N_cores]={};
-	int spt = 1e8;//sample per thread
+	int spt = 1e7;//sample per thread
 	int ID,i;
         #pragma omp parallel shared(sum,spt) private(ID,i)
         {
                 ID = omp_get_thread_num();
-		std::default_random_engine e((unsigned int)time(0)+ID);
-  		std::uniform_real_distribution<double> distribution(0.0,1.0);
+			std::default_random_engine e((unsigned int)time(0)+ID);
+  			std::uniform_real_distribution<double> distribution(0.0,1.0);
                 //int x[1000];
                 //initialization
                 for(i=0;i<spt;i++){
-                        double x = distribution(e);
-			double y = distribution(e);
-			//cout<<"L0-->core:"<< ID <<"    "<<x<<","<<y<<std::endl;
-			if(x*x+y*y<1.0){
-				sum[ID]++;
-			}
+                    double x = distribution(e);
+					double y = distribution(e);
+					//cout<<"L0-->core:"<< ID <<"    "<<x<<","<<y<<std::endl;
+					if(x*x+y*y<1.0){
+						sum[ID]++;
+					}
                 }
 		#pragma omp critical
                 cout<<"L0-->core:"<< ID <<", sum:"<< sum[ID]<<std::endl;
