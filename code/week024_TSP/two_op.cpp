5557ec566680a5ccc1fc76f471ae0976e0827ffd
wasit7 <wasit7@gmail.com>
2015-08-27 18:53:32 +0700

    debugging cout is removed

--- code/week024_TSP/two_op.cpp
+++ code/week024_TSP/two_op.cpp
@@ -4,6 +4,8 @@
 #include <iomanip>
 #include <time.h>
 #include <stdlib.h>
+#include <omp.h>
+#define Ncores 8
 using namespace std;
 float subDistance(int N,int* path, float* A,int i){
 //0<=i and i<N
@@ -18,9 +20,17 @@ float totalDistance(int N, int* path, float* A){
     }
     return td;
 }
-void twoOpt(int N, int* path, float* A){
+void showPath(int N, int* path, float* A){
+    cout<<"    Path: ";
+    for(int i=0; i<N; i++){
+        cout<< path[i]<<"->";
+    }
+    cout<<endl<<"    Total distance: "<<totalDistance(N,path,A)<<endl;
+}
+void twoOpt(int N, int* path, float* A,int M){
     int na,nb,nc,nx,ny,nz,i,j;
-    for(int k=0; k<8e7; k++){
+    srand (time(NULL));
+    for(int k=0; k<M; k++){
         i=rand() % (N-1)+1;
         j=rand() % (N-1)+1;
         //problem i and j may be the same value and makes swapping is useless
@@ -37,12 +47,66 @@ void twoOpt(int N, int* path, float* A){
         }
     }
 }
+void twoOpt_omp(int N, int* path, float* A,int M){
+
+    float* ptd=new float[Ncores];//parallel total distance
+    int** ppath=new int* [Ncores];
+    for(int i=0; i<Ncores; i++){
+        ppath[i]=new int[N];
+        for(int j=0; j<N; j++){
+            ppath[i][j]=path[j];
+        }
+    }
+
+    #pragma omp parallel shared(A,N,ppath,M)
+    {
+
+        //--a--b--c--
+        //--x--y--z--
+        //swapping between b and y
+
+        int na,nb,nc,nx,ny,nz,i,j,ID;
+        ID = omp_get_thread_num();
+        srand (ID);
+        int* path=ppath[ID];
+        for(int k=0; k<M; k++){
+            i=rand() % (N-1)+1;
+            j=rand() % (N-1)+1;
+            //problem i and j may be the same value and makes swapping is useless
+            na=path[(i-1+N)%N];
+            nb=path[i];
+            nc=path[(i+1)%N];
+            nx=path[(j-1+N)%N];
+            ny=path[j];
+            nz=path[(j+1)%N];
+            //swap ci and rj
+            if( A[na*N+ny]+A[ny*N+nc]+A[nx*N+nb]+A[nb*N+nz] < A[na*N+nb]+A[nb*N+nc]+A[nx*N+ny]+A[ny*N+nz]){
+                path[i]=ny;
+                path[j]=nb;
+            }
+        }
+        ptd[ID]=totalDistance(N,ppath[ID],A);
+        //#pragma omp critical
+        //cout<<"ID:"<<ID<<" ptd:"<<ptd[ID]<<endl;
+
+    }
+
+    //find min path from all cores
+    int bestcore=0;
+    for(int i=1;i<Ncores;i++){
+        if(ptd[i]<ptd[bestcore]){
+            bestcore=i;
+        }
+    }
+    for(int i=0;i<N;i++){
+        path[i]=ppath[bestcore][i];
+    }
+
+}
 int main(){
     string fname="data2.tsv";
     ifstream file(fname);
     int N;
-    int* path;
-    srand (time(NULL));
     if(file.is_open()){
         cout<<"Loading "<<fname<<endl;
         file >> N;
@@ -54,28 +118,8 @@ int main(){
                 file >> A[r*N+c];
             }
         }
-
-        cout<<"Array A: "<<endl;
-        for(int r = 0; r < N; ++r){
-            for(int c = 0; c < N; ++c){
-                cout <<"\t"<<setw(4)<<fixed <<setprecision(1)<<right<< A[r*N+c];
-            }
-            cout<<endl;
-        }
-
-        cout<<"Path: ";
-        for(int i=0; i<N; i++){
-            cout<< path[i]<<"-->";
-            cout<< subDistance(N,path,A,i)<<endl;
-        }
-        cout<<endl<<"Total distance: "<<totalDistance(N,path,A)<<endl;
-
-        cout<<">>>twoOpt";
-        twoOpt(N, path, A);
-        cout<<"Path: ";
-        for(int i=0; i<N; i++){
-            cout<< path[i]<<"-->";
-        }
-        cout<<endl<<"Total distance: "<<totalDistance(N,path,A)<<endl;
+        cout<<">>>twoOpt"<<endl;
+        twoOpt(N, path, A,8e7);
+        showPath(N,path,A);
     }
 }
--- code/week024_TSP/two_op_omp.cpp
+++ code/week024_TSP/two_op_omp.cpp
@@ -27,7 +27,27 @@ void showPath(int N, int* path, float* A){
     }
     cout<<endl<<"    Total distance: "<<totalDistance(N,path,A)<<endl;
 }
-void twoOpt(int N, int* path, float* A){
+void twoOpt(int N, int* path, float* A,int M){
+    int na,nb,nc,nx,ny,nz,i,j;
+    srand (time(NULL));
+    for(int k=0; k<M; k++){
+        i=rand() % (N-1)+1;
+        j=rand() % (N-1)+1;
+        //problem i and j may be the same value and makes swapping is useless
+        na=path[(i-1+N)%N];
+        nb=path[i];
+        nc=path[(i+1)%N];
+        nx=path[(j-1+N)%N];
+        ny=path[j];
+        nz=path[(j+1)%N];
+        //swap ci and rj
+        if( A[na*N+ny]+A[ny*N+nc]+A[nx*N+nb]+A[nb*N+nz] < A[na*N+nb]+A[nb*N+nc]+A[nx*N+ny]+A[ny*N+nz]){
+            path[i]=ny;
+            path[j]=nb;
+        }
+    }
+}
+void twoOpt_omp(int N, int* path, float* A,int M){
 
     float* ptd=new float[Ncores];//parallel total distance
     int** ppath=new int* [Ncores];
@@ -38,7 +58,7 @@ void twoOpt(int N, int* path, float* A){
         }
     }
 
-    #pragma omp parallel shared(A,N,ppath)
+    #pragma omp parallel shared(A,N,ppath,M)
     {
 
         //--a--b--c--
@@ -48,28 +68,26 @@ void twoOpt(int N, int* path, float* A){
         int na,nb,nc,nx,ny,nz,i,j,ID;
         ID = omp_get_thread_num();
         srand (ID);
-
-        for(int k=0; k<1e7; k++){
+        int* path=ppath[ID];
+        for(int k=0; k<M; k++){
             i=rand() % (N-1)+1;
             j=rand() % (N-1)+1;
             //problem i and j may be the same value and makes swapping is useless
-            na=ppath[ID][(i-1+N)%N];
-//            #pragma omp critical
-//            cout<<"debug"<<na<<endl;
-            nb=ppath[ID][i];
-            nc=ppath[ID][(i+1)%N];
-            nx=ppath[ID][(j-1+N)%N];
-            ny=ppath[ID][j];
-            nz=ppath[ID][(j+1)%N];
+            na=path[(i-1+N)%N];
+            nb=path[i];
+            nc=path[(i+1)%N];
+            nx=path[(j-1+N)%N];
+            ny=path[j];
+            nz=path[(j+1)%N];
             //swap ci and rj
             if( A[na*N+ny]+A[ny*N+nc]+A[nx*N+nb]+A[nb*N+nz] < A[na*N+nb]+A[nb*N+nc]+A[nx*N+ny]+A[ny*N+nz]){
-               ppath[ID][i]=ny;
-               ppath[ID][j]=nb;
+                path[i]=ny;
+                path[j]=nb;
             }
         }
         ptd[ID]=totalDistance(N,ppath[ID],A);
-        #pragma omp critical
-        cout<<"ID:"<<ID<<" ptd:"<<ptd[ID]<<endl;
+        //#pragma omp critical
+        //cout<<"ID:"<<ID<<" ptd:"<<ptd[ID]<<endl;
 
     }
 
@@ -89,7 +107,6 @@ int main(){
     string fname="data2.tsv";
     ifstream file(fname);
     int N;
-    int* path;
     if(file.is_open()){
         cout<<"Loading "<<fname<<endl;
         file >> N;
@@ -101,21 +118,8 @@ int main(){
                 file >> A[r*N+c];
             }
         }
-
-        cout<<"Array A: "<<endl;
-        for(int r = 0; r < N; ++r){
-            for(int c = 0; c < N; ++c){
-                cout <<"\t"<<setw(4)<<fixed <<setprecision(1)<<right<< A[r*N+c];
-            }
-            cout<<endl;
-        }
-
-
-        showPath(N,path,A);
         cout<<">>>twoOpt"<<endl;
-        for(int i=0;i<1e0;i++){
-            twoOpt(N, path, A);
-            showPath(N,path,A);
-        }
+        twoOpt_omp(N, path, A,1e7);
+        showPath(N,path,A);
     }
 }
