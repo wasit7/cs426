cedf454406686a70fc8a198bc3b507966ab27970
Wasit <wasit7@gmail.com>
2015-09-04 01:44:48 +0700

    pbs ipcluster/start.sh

--- ipcluster/start.sh
+++ ipcluster/start.sh
@@ -1,5 +1,6 @@
-export conjson=`echo ${HOME}/.config/ipython/profile_default/security/ipcontroller-engine.json`
-export engjson=`echo ${USER}@10.100.20.201:${HOME}/.ipython/profile_default/security`
+export IPYTHONDIR=~/.ipython
+export conjson=`echo ${IPYTHONDIR}/profile_default/security/ipcontroller-engine.json`
+export engjson=`echo ${USER}@10.100.20.201:${IPYTHONDIR}/profile_default/security`
 screen -ls | grep ipc | cut -d. -f1 | awk '{print $1}' | xargs kill
 screen -S ipc -d -m ./controller.sh
 ./engine.sh
